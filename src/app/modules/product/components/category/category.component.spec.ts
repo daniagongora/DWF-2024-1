d03de96c3323ce1c273dd8a161ec83fa861d59f8
daniagongora <dania1012@ciencias.unam.mx>
2023-10-04 19:54:57 -0600

    modal feauture and add a category feature

--- src/app/modules/product/components/category/category.component.spec.ts
+++ src/app/modules/product/components/category/category.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CategoryComponent } from './category.component';
+import { FormBuilder, Validators } from '@angular/forms';
+
+declare var $: any;
 
 describe('CategoryComponent', () => {
   let component: CategoryComponent;
@@ -19,3 +22,4 @@ describe('CategoryComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
