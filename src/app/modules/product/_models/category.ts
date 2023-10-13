2fe7e2f7451ba29126986f7c6a23aeadda79a2a1
daniagongora <dania1012@ciencias.unam.mx>
2023-10-12 20:12:49 -0600

    add Mas column and error

--- src/app/modules/product/_models/category.ts
+++ src/app/modules/product/_models/category.ts
@@ -2,9 +2,9 @@ export class Category{
     category_id: number = 0;
     code: string = "";
     category: string = "";
-    status: string = "";
+    status: number = 0;
 
-    constructor(category_id: number, code: string, category: string, status: string){
+    constructor(category_id: number, code: string, category: string, status: number){
         this.category_id = category_id;
         this.code = code;
         this.category = category;
--- src/app/modules/product/components/category/category.component.ts
+++ src/app/modules/product/components/category/category.component.ts
@@ -11,7 +11,8 @@ declare var $: any;
 })
 export class CategoryComponent implements OnInit {
   categories: Category[] = []; // Declarar un arreglo de objetos Category
-
+  categoryUpdated: number = 0;
+  
   form = this.formBuilder.group({
     category: ["", [Validators.required]],
     code: ["", [Validators.required]],
@@ -27,13 +28,34 @@ export class CategoryComponent implements OnInit {
     this.getCategories();
   }
 
+  disableCategory(id: number){
+    for(let category of this.categories){
+      if(category.category_id == id){
+        category.status = 0;
+        alert("Postre sin entregar!");
+        break;
+      }
+    }
+    console.log("SALIR")
+  }
+
+  enableCategory(id: number){
+    for(let category of this.categories){
+      if(category.category_id == id){
+        category.status = 1;
+        alert("Postre entregado exitosamente!");
+        break;
+      }
+    }
+  }
+
   getCategories() {
     // Agrega tres objetos Category con datos dummy al arreglo categories
-    let category1 = new Category(1087, "50192404", 'Fluffy', 'Sent');
-    let category2 = new Category(3456, "50182002", 'Butter', 'Confirmed');
-    let category3 = new Category(3346, "50182001", 'Butter', 'Cancelled');
-    let category4 = new Category(3345, "50182000", 'Fermented', 'Confirmed');
-    let category5 = new Category(5679, "50181906", 'Oil', 'Sent');
+    let category1 = new Category(1087, "50192404", 'Pastel', 1);
+    let category2 = new Category(3456, "50182002", 'Brownie', 0);
+    let category3 = new Category(3346, "50182001", 'Muffin', 1);
+    let category4 = new Category(3345, "50182000", 'Carlota', 0);
+    let category5 = new Category(5679, "50181906", 'Flan', 1);
 
     // Agrega los objetos Category al arreglo categories
     this.categories.push(category1);
@@ -44,25 +66,70 @@ export class CategoryComponent implements OnInit {
   }
 
   onSubmit(){
+    if(this.categoryUpdated == 0){
+      this.onSubmitCreate();
+    }else{
+      this.onSubmitUpdate();
+    }
+  }
+
+  onSubmitCreate(){
     this.submitted = true;
 
     if(this.form.invalid) return;
 
     this.submitted = false;
 
-    let category = new Category(0, this.form.controls['code'].value!, this.form.controls['category'].value!, " ");
+    let category = new Category(0, this.form.controls['code'].value!, this.form.controls['category'].value!, 1);
+    console.log(this.form.value);
     this.categories.push(category);
     
     $("#modalForm").modal("hide");
 
-    alert("Categoria guardada exitosamente!");
+    alert("Postre guardada exitosamente!");
 
   }
 
-  showModalForm(){
-    console.log("ayudaaaaaaaaaa D:");
+  onSubmitUpdate(){
+    this.submitted = true;
+
+    if(this.form.invalid) return;
+
+    this.submitted = false;
+
+    for(let category of this.categories){
+      if(category.category_id == this.categoryUpdated){
+        category.code = this.form.controls['code'].value!;
+        category.category = this.form.controls['category'].value!;
+        break;
+      }
+    }
+    
+    $("#modalForm").modal("hide");
+
+    alert("Postre actualizada exitosamente!");
+
+    this.categoryUpdated = 0;
+
+  }
+
+  updateCategory(category: Category){
+    this.categoryUpdated = category.category_id;
+    
     this.form.reset();
+    this.form.controls['category'].setValue(category.code);
+    this.form.controls['code'].setValue(category.category);
+    
     this.submitted = false;
     $("#modalForm").modal("show");
   }
+
+  showModalForm(){
+    this.form.reset();
+    this.categoryUpdated = 0;
+    this.submitted = false;
+    $("#modalForm").modal("show");
+  }
+
+  
 }
